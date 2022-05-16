3dcd937e1316c1d66b0d3e5c6306758809bad950
logicopslab <logicopslab@gmail.com>
2022-05-17 00:51:00 +0530

    Day 12

---
+++ Day 12/01.AddMatrices.py
@@ -0,0 +1,28 @@
+# Program to add two matrices
+
+M1 = [[10,100,230],
+	[40,50,60],
+	[70,80,90]]
+
+M2 = [[90,100,70],
+	[60,50,40],
+	[30,20,10]]
+
+M3 = [[0,0,0],
+	[0,0,0],
+	[0,0,0]]
+
+# iterating rows
+
+for i in range(len(M1)):
+
+# iterating columns
+	
+	for j in range(len(M2)):
+		M3[i][j] = M1[i][j] + M2[i][j]
+		print("i is", i, "j is", j)
+		print(M3)
+
+print("Final Matrix is :")
+for k in M3:
+	print(k)
